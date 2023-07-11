6193a6014f2cccf314f06a87f5efe9233987ddc2
Kushal D Jain <jkushal069@gmail.com>
2023-07-11 15:19:10 +0530

    Add files via upload

---
+++ server/middlewares/AuthMiddleware.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.jwt;
+  const n = JSON.parse(token);
+  console.log(n.jwt);
+  if (!n) return res.status(401).send("You are not authenticated!");
+  jwt.verify(n.jwt, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return res.status(403).send("Token is not valid!");
+    req.userId = payload?.userId;
+    next();
+  });
+};
