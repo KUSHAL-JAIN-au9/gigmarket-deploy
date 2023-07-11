fc4c31346dae1528089abf137687ba5a3130c735
Kushal Jain <jkushal069@gmail.com>
2023-07-11 20:32:15 +0530

    removed headers for getting invalid jwt

--- client/src/components/AuthWrapper.jsx
+++ client/src/components/AuthWrapper.jsx
@@ -36,7 +36,7 @@ function AuthWrapper({ type }) {
         } = await axios.post(
           type === "login" ? LOGIN_ROUTE : SIGNUP_ROUTE,
           { email, password },
-          { headers: { "Access-Control-Allow-Origin": "*" } },
+          // { headers: { "Access-Control-Allow-Origin": "*" } },
           { withCredentials: true }
         );
         setCookies("jwt", { jwt: jwt });
